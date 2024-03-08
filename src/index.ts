06e105287507e34a785adca7759f7bffd4326af9
Ethansnow2012 <ethansnow2012@gmail.com>
2024-03-08 16:20:18 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface FileData {
+    path: string;
+    content: string;
+}
+
+function readFilesRecursively(dir: string): FileData[] {
+    let results: FileData[] = [];
+
+    fs.readdirSync(dir).forEach(file => {
+        file = path.resolve(dir, file);
+        const stat = fs.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(readFilesRecursively(file));
+        } else {
+            results.push({
+                path: file,
+                content: fs.readFileSync(file, 'utf8')
+            });
+        }
+    });
+
+    return results;
+}
+
+const dirPath = './src'; // Replace with your directory path
+const filesData = readFilesRecursively(dirPath);
+const jsonContent = JSON.stringify(filesData, null, 2);
+
+fs.writeFileSync('output.json', jsonContent); // This writes the JSON data to output.json
+console.log('Files data written to output.json');
