b6b53da394b049a462fe1ffec0b5c89b11bd129d
Ethansnow2012 <ethansnow2012@gmail.com>
2024-03-12 19:15:03 +0800

    feat: respecting config file and gitignore

---
+++ cbj.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dirPath: './',
+  outputFileName: 'cbj_output.json'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,56 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
 import * as path from 'path';
+import ignore from 'ignore';
+
+let cbjConfig = {
+  dirPath: './',
+  outputFileName: 'cbj_output.json'
+};
+
+type Ignore = ReturnType<typeof ignore>;
+
+try {
+    const configPath = process.cwd()+'\\cbj.config.js'
+    console.log('trying to read configModule:', configPath);
+    const configModule = require(configPath);
+    if(configModule){
+        console.log('Config module read.');
+    }
+    cbjConfig = configModule || cbjConfig;
+    console.log('Config module read.', cbjConfig);
+    
+} catch (error) {
+  console.log('No cbj.config.ts found, using default configuration.', error);
+}
 
 interface FileData {
     path: string;
     content: string;
 }
 
-function readFilesRecursively(dir: string): FileData[] {
+function readGitignore(): string[] {
+    const forceIgnorePath = ['.git', '.gitignore', 'node_modules', 'dist', 'build', 'out', 'coverage', 'cbj_output.json', 'cbj.config.js', '*-lock.yaml']
+    const gitignorePath = '.gitignore' //path.join('/', '.gitignore');
+    console.log('reading gitignorePath:', gitignorePath);
+    if (fs.existsSync(gitignorePath)) {
+        return forceIgnorePath.concat(fs.readFileSync(gitignorePath, 'utf8').split('\n'));
+    }
+    return forceIgnorePath;
+}
+
+function readFilesRecursively(dir: string, ig: Ignore): FileData[] {
     let results: FileData[] = [];
 
     fs.readdirSync(dir).forEach(file => {
         file = path.resolve(dir, file);
+        if (ig.ignores(path.relative(cbjConfig.dirPath, file))) {
+            return;
+        }
         const stat = fs.statSync(file);
 
         if (stat && stat.isDirectory()) {
-            results = results.concat(readFilesRecursively(file));
+            results = results.concat(readFilesRecursively(file, ig));
         } else {
             results.push({
                 path: file,
@@ -27,9 +62,14 @@ function readFilesRecursively(dir: string): FileData[] {
     return results;
 }
 
-const dirPath = './src'; // Replace with your directory path
-const filesData = readFilesRecursively(dirPath);
+const ig = ignore();
+const gitignoreRules = readGitignore()
+ig.add(gitignoreRules);
+
+const dirPath = cbjConfig.dirPath;
+console.log('Read files recursively from:', dirPath);
+const filesData = readFilesRecursively(dirPath, ig);
 const jsonContent = JSON.stringify(filesData, null, 2);
 
-fs.writeFileSync('cbj_output.json', jsonContent); // This writes the JSON data to output.json
-console.log('Files data written to output.json');
+fs.writeFileSync(cbjConfig.outputFileName, jsonContent);
+console.log(`Files data written to ${cbjConfig.outputFileName}`);
